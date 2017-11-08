94234b0f1fce7792704b4e1fb7c45d981803f58b
willbenmitch <ben.mitchell01@gmail.com>
2017-11-08 17:13:08 -0500

    update FlatList

--- src/FlatList.js
+++ src/FlatList.js
@@ -2,8 +2,25 @@
 import React, { PureComponent } from 'react'
 import { FlatList } from 'react-native'
 
-export default class FlatListPureComponent extends PureComponent {
-	keyExtractor = (item: { id: string }) => item.id
+// intentionally left absolute "|" symbols out of type definition
+type FlatListProps = {
+	data: any[],
+	renderItem: () => any,
+	getItemCount?: () => any,
+	getItem?: () => any,
+	extraData?: any,
+	onEndReached?: () => void,
+	onEndReachThreshold?: number,
+	contentContainerStyle?: any,
+	horizontal?: bool,
+	onScroll?: () => any,
+	style?: any,
+	keyExtractor: () => any,
+}
+
+export default class FlatListPureComponent extends PureComponent<FlatListProps> {
+	keyExtractor = this.props.keyExtractor ? this.props.keyExtractor 
+		: (item: { id: string }) => item.id
 
 	render() {
 		return (
