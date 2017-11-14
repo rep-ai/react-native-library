806451f9b2e5cfee1d933036422af0508e53008f
Nicholas Tancredi <nicholastancredi@gmail.com>
2017-11-14 15:25:06 -0500

    Updated FlatList

--- src/FlatList.js
+++ src/FlatList.js
@@ -1,16 +1,47 @@
 //@flow
 import React, { PureComponent } from 'react'
 import { FlatList } from 'react-native'
+import checkType from 'check-types'
 
-export default class FlatListPureComponent extends PureComponent {
-	keyExtractor = (item: { id: string }) => item.id
+// NOTE: intentionally left absolute "|" symbols out of type definition
+type FlatListProps = {
+    data: any[],
+    renderItem: () => any,
+    getItemCount?: () => any,
+    getItem?: () => any,
+    extraData?: any,
+    onEndReached?: () => void,
+    onEndReachThreshold?: number,
+    contentContainerStyle?: any,
+    horizontal?: bool,
+    onScroll?: () => any,
+    style?: any,
+    keyExtractor?: (item: { id: string }, i: number) => string,
+}
 
-	render() {
-		return (
+export default class FlatListPureComponent extends PureComponent<FlatListProps> {
+    keyExtractor = (item: { id: string }, i: number): string => {
+        const { keyExtractor } = this.props
+
+        if (typeof keyExtractor === 'function') {
+            return keyExtractor(item, i)
+        }
+
+        if (checkType.object(item)) {
+            if (typeof item.id === 'string') {
+                return item.id
+            }
+        }
+
+        return i.toString()
+    }
+
+    render() {
+        return (
             <FlatList
                 keyExtractor={this.keyExtractor}
                 {...this.props}
             />
-		)
-	}
+        )
+    }
 }
