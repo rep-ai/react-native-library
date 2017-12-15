8a8437bb4fbd75147b8c33dadf305efba3f6534a
Nicholas Tancredi <nicholastancredi@gmail.com>
2017-12-15 18:16:45 -0400

    Updated OrdinalSuffix

--- src/UtilityMethods/ordinalSuffix.js
+++ src/UtilityMethods/ordinalSuffix.js
@@ -1,16 +1,30 @@
 // @flow
-export default (number: number) => {
+export type OrdinalSuffixResult = 'st' | 'nd' | 'rd' | 'th' | ''
+
+export default (number: number): OrdinalSuffixResult => {
+    if (typeof number !== 'number') {
+        console.warn(
+            `Warning: The function ordinalSuffix requires its first and only argument to be a number. \n`,
+            `Instead it was provided the value ${number} which is a typeof ${typeof number}%c.`
+        )
+
+        return ''
+    }
+
     const j = number % 10
     const k = number % 100
 
     if (j === 1 && k !== 11) {
-        return number + 'st'
+        return 'st'
     }
+
     if (j === 2 && k !== 12) {
-        return number + 'nd'
+        return 'nd'
     }
+
     if (j === 3 && k !== 13) {
-        return number + 'rd'
+        return 'rd'
     }
-    return number + 'th'
+
+    return 'th'
 }
